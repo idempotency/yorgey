5027f45a3c7386c3e8551bc2e958c5c213fbf965
idempotency <ryan.pollard@ephox.com>
2017-06-05 15:38:49 +1000

    Played with some lecture functions

---
+++ week-3/Golf.hs
@@ -0,0 +1,35 @@
+module Golf where
+
+data IntList = Empty
+             | Cons Int IntList
+  deriving Show
+
+data List t = E
+            | C t (List t)
+  deriving Show
+
+doubleAll :: IntList -> IntList
+doubleAll Empty        = Empty
+doubleAll (Cons x xss) = Cons (x*2) $ doubleAll xss
+
+intListMap :: IntList -> (Int -> Int) -> IntList
+intListMap Empty _        = Empty
+intListMap (Cons x xss) f = Cons (f x) (intListMap xss f)
+
+intListFilter :: IntList -> (Int -> Bool) -> IntList
+intListFilter Empty _        = Empty
+intListFilter (Cons x xss) f | f x       = Cons x (intListFilter xss f)
+                             | otherwise = intListFilter xss f
+
+listMap :: List a -> (a -> b) -> List b
+listMap E _       = E
+listMap (C x xss) f = C (f x) (listMap xss f)
+
+listFilter :: List a -> (a -> Bool) -> List a
+listFilter E _      = E
+listFilter (C x xss) f | f x       = C x (listFilter xss f)
+                       | otherwise = listFilter xss f
+
+skips :: [a] -> [[a]]
+skips []         = []
+skips xs@(x:xss) = [xs]
